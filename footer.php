5e7d871e6d7408b994473882e1bf95e972482b2c
Pierre-Antoine Rouby <pa.rouby@gmail.com>
2018-01-30 15:24:32 +0100

    update legal information

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer>
   <div id="footer">
-    <center>
-      Licence : <a href="/LICENCE"
-		   alt="Licence du site"
-		   class="footer"
-		   >AGPLv3</a></br>
-      Source code : <a href="https://github.com/RoubyPA/Portfolio"
-		       alt="Code source de cette page web"
-		       class="footer">
-	GitHub</a></br>
-      Powered by <a href="https://www.kimsufi.com/" class="footer">kimsufi.com</a>
-    </center>
+    Licence : <a href="/LICENCE"
+		 alt="Licence du site"
+		 class="footer"
+		 >AGPLv3</a></br>
+    Source code : <a href="https://github.com/RoubyPA/Portfolio"
+		     alt="Code source de cette page web"
+		     class="footer">
+      GitHub</a></br>
+    Powered by <a href="https://www.ovh.com/"
+		  class="footer">
+      OVH (kimsufi)</a> in France
   </div>
 </footer>
