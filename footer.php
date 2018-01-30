0a9fb98c257aad1f761a5fccccba945bcf9403c5
Pierre-Antoine Rouby <pa.rouby@gmail.com>
2018-01-30 16:26:36 +0100

    change legal mension

--- footer.php
+++ footer.php
@@ -7,9 +7,10 @@
     Source code : <a href="https://github.com/RoubyPA/Portfolio"
 		     alt="Code source de cette page web"
 		     class="footer">
-      GitHub</a></br>
-    Powered by <a href="https://www.ovh.com/"
-		  class="footer">
-      OVH (kimsufi)</a> in France
+      GitHub</a><br>
+    <br>
+    Site hébergé par<a href="https://www.ovh.com" class="footer"> OVH</a>,<br>
+    tel: 09 72 10 10 10 <br>
+    2 rue Kellermann - 59100 Roubaix - France <br>
   </div>
 </footer>
