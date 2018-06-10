c86acd1293599618e83d86003ac672a7c3eace64
Pierre-Antoine Rouby <contact@parouby.fr>
2018-06-10 12:20:35 +0200

    Portfolio: Update source code link.

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 		 alt="Licence du site"
 		 class="footer"
 		 >AGPLv3</a></br>
-    Source code : <a href="https://github.com/RoubyPA/Portfolio"
-		     alt="Code source de cette page web"
+    Source code : <a href="https://framagit.org/prouby/Portfolio"
+		     alt="Code source de ce site web."
 		     class="footer">
-      GitHub</a><br>
+      Framagit</a><br>
     <br>
     Site hébergé par<a href="https://www.ovh.com" class="footer"> OVH</a>,<br>
     tel: 09 72 10 10 10 <br>
