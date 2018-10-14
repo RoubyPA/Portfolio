652dbf77ee6bf36b5fc5835c0ccd5bdee1021d39
Pierre-Antoine Rouby <contact@parouby.fr>
2018-10-14 12:05:04 +0200

    blog: Add blog page.
    * blog.php: New file.
    * nav.php: Add blog.php in nav.

---
+++ blog.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>
+      Pierre-Antoine Rouby, page professionnelle :
+      Logiciel Libre, Linux, Programmation
+    </title>
+    <meta name="description"
+	  content="Site profesionelle de Pierre-Antoine Rouby, informaticien." />
+    <meta name="keywords" content="Logiciel Libre, Linux, Programmation,
+				   Netralité du Net, Open Data" />
+    <link rel="stylesheet" href="css/main.css" />
+  </head>
+
+  <!-- Body -->
+  <body>
+
+    <!-- Insert header -->
+    <?php include('header.php'); ?>
+
+    <!-- Insert nav -->
+    <?php include('nav.php') ?>
+  
+    <div class="main">
+      <div class="article">
+        <h2>Création d'un mode emacs (efetch)</h2>
+        <p>Vous pouvez retouver
+          <a href="blog/create_emacs_mode.html">ici</a> mon première
+          article de blog sur la création du mode emacs
+          (<a href="https://framagit.org/prouby/emacs-fetch">efetch</a>).
+        </p>
+	<a href="blog/create_emacs_mode.html"
+	   alt="Blog création d'un mode emacs.">
+          <img class="bg" src="blog/efetch_01.png" />
+	</a>
+      </div>
+
+      
+    <!-- Insert footer -->
+    <?php include('footer.php'); ?>
+    
+  </body>
+</html>
--- nav.php
+++ nav.php
@@ -1,5 +1,6 @@
 <nav id="nav">
   <a href='index.php' rel="follow" class="block">Home</a>
   <a href='cv.php' rel="nofollow" class="block">CV</a>
+  <a href='blog.php' rel="nofollow" class="block">Blog</a>
   <a href='contact.php' rel="nofollow" class="block">Contact</a>
 </nav>
