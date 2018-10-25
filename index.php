2d505aaf4474d7dc69285c4a9679e45640d37f28
Pierre-Antoine Rouby <contact@parouby.fr>
2018-10-25 10:20:16 +0200

    Portfolio: Add "Mes projets".
    * index.php: Add "Mes projets".

--- index.php
+++ index.php
@@ -8,31 +8,31 @@
     </title>
     <meta name="description"
 	  content="Site profesionelle de Pierre-Antoine Rouby, informaticien." />
-    <meta name="keywords" content="Logiciel Libre, Linux, Programmation, 
+    <meta name="keywords" content="Logiciel Libre, Linux, Programmation,
 				   Netralité du Net, Open Data" />
     <link rel="stylesheet" href="css/main.css" />
   </head>
-  
+
   <!-- Body -->
   <body>
-    
+
     <!-- Insert header -->
     <?php include('header.php'); ?>
 
     <!-- Insert nav -->
     <?php include('nav.php') ?>
-      
+
     <div class="main">
       <div class="article">
 	<h2>Présentation</h2>
 	<h3>Bonjour, je m'appelle Rouby Pierre-Antoine, j'ai 23 ans.</h3>
-	<p>Je suis actuellement en licence professionnelle 
+	<p>Je suis actuellement en licence professionnelle
 	  <a href="http://dept-info.labri.fr/ENSEIGNEMENT/adsillh/"
 	     alt="Site web de la licence ADSILLH">ADSILLH
 	  </a>(Administrateur et Développeur de Systèmes Informatiques à base
 	  de Logiciels Libres et Hybrides).
 	</p>
-	
+
 	<p>Je suis passionnée par l'informatique depuis des années, et
 	  utilisateur de système UNIX (GNU/Linux) depuis presque aussi
 	  longtemps.
@@ -47,7 +47,7 @@
 	<p>Je suis aussi développeur, je fait principalement du C et je mes
 	  mes projet en libre sur mon compte GitHub.
 	</p>
-	
+
 	<p>J'ai commencer à apprendre à codé et à utilisé des système Unix
 	  avant d'intégrer une formation en informatique. J'ai donc commencer
 	  par m'autoformer via internet. C'est à cette période que j'ai
@@ -73,7 +73,7 @@
           </li>
 	  <li>Cette année 2018 je serrais au
 	    <a href="https://fosdem.org/2018/"
-	       alt="Fosdem 2018 - Rencontre pour les developper de logiciel 
+	       alt="Fosdem 2018 - Rencontre pour les developper de logiciel
 		    libre et open source">
 	      FOSDEM</a> le 3 et le 4 février.
 	  </li>
@@ -81,7 +81,7 @@
       </div>
 
       </br>
-      
+
       <div class="split_article">
 	<h2>Éthique</h2>
 	<ul>
@@ -101,7 +101,7 @@
 	    alt="April - Promouvoir et soutenir le logiciel libre" />
 	</a>
       </div>
-      
+
       <div class="split_article">
 	<h2>Mes contributions</h2>
 	<ul>
@@ -118,6 +118,22 @@
       </div>
 
       <div class="split_article">
+	<h2>Mes projets</h2>
+	<ul>
+          <li><a href="https://framagit.org/prouby/emacs-fetch"
+		 alt="Projet emacs-fetch">
+	      Emacs-fetch</a>
+	  </li>
+
+	  <li><a href="https://framagit.org/prouby/emacs-spyglass"
+		 alt="Projet emacs-spyglass">
+	      Spyglass servers monitoring</a>
+	  </li>
+	</ul>
+      </div>
+
+
+      <div class="split_article">
 	<h2>Mon CV</h2>
 	<ul>
 	  <li><a href="cv.php"
@@ -127,9 +143,9 @@
 	</ul>
       </div>
 
-      
+
     <!-- Insert footer -->
     <?php include('footer.php'); ?>
-    
+
   </body>
 </html>
