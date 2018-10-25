dd15d46ffb9c693143514bb9e4cc8ab050f83481
Pierre-Antoine Rouby <contact@parouby.fr>
2018-10-25 10:06:55 +0200

    Portfolio: Image banner.
    * css/main.css: Remove body image background.
    * header.php: Add banner image, remove name.
    * img/lambda.png: New file.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
   <div class="banner">
-    <!-- <img src="img/banner.png" alt="banner"></img> -->
-    <center>Pierre-Antoine <strong>Rouby</strong></center>
+    <center><img src="img/lambda.png" alt="banner" width=150px></img></center>
+    <!-- <center>Pierre-Antoine <strong>Rouby</strong></center> -->
   </div>
 </header>
